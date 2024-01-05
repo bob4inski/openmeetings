a46fb23ca5e7a54b22e199b6a0e4cb73af5d7518
bob4inski <robertstryzhak@yandex.ru>
2024-01-05 16:50:53 +0000

    congratulations, we starts and have error!

---
+++ om/scripts/om_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# #############################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# #############################################
+
+echo "OM server of type ${OM_TYPE} will be set-up"
+echo "session required pam_limits.so" >> /etc/pam.d/common-session
